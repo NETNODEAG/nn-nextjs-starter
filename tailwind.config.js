1a5b463e39aadbf11251ec74aee953ee7ed10fda
Natalia Justicia <nataliajusticia@gmail.com>
2023-05-03 15:06:56 +0200

    Install postcss-import + theme files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -15,4 +16,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
