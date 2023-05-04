1d099f1939941fb6c114302b2f11310902f24bf9
Natalia Justicia <nataliajusticia@gmail.com>
2023-05-04 08:43:48 +0200

    Remove unused packages

--- src/components/node/node-page/NodePage.tsx
+++ src/components/node/node-page/NodePage.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import Link from 'next/link';
 import { DrupalNode, DrupalParagraph } from 'next-drupal';
 
 import Paragraph from '@/components/paragraph/Paragraph';
