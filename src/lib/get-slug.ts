7fa5a3d1ad69206f5c0f99fe9d8019c3ca988c48
Natalia Justicia <nataliajusticia@gmail.com>
2023-05-08 09:06:23 +0200

    Improve get resource by slug method

--- src/lib/get-slug.ts
+++ src/lib/get-slug.ts
@@ -11,17 +11,24 @@ const API_URL = `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/router/translate-pat
  * @return {Promise} Promise object represents a single resource
  */
 export async function getResourceBySlug(slug: Array<string>) {
-  let jsonApiUrl;
+  // Build the JSON API URL based on the slug array
+  const jsonApiUrl = `${API_URL}/${slug.join('/')}/?format=json_api`;
 
-  if (slug.length > 1) {
-    jsonApiUrl = API_URL + slug.join('/') + '?format=json_api';
-  } else {
-    jsonApiUrl = API_URL + slug + '?format=json_api';
+  try {
+    // Fetch the data from the API URL
+    const response = await fetch(jsonApiUrl.toString());
+    const resource = await response.json();
+
+    // Check if the resource was resolved or not
+    if (!resource.resolved) {
+      return null;
+    }
+
+    // If the resource was resolved, return the data
+    return resource;
+  } catch (error) {
+    throw new Error(
+      `Error fetching resource with slug ${slug.join('/')}: ${error}`
+    );
   }
-
-  const response = await fetch(jsonApiUrl.toString());
-
-  const data = await response.json();
-
-  return data;
 }
